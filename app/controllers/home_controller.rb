b55dbbbfe28d8708c94fe2d12e8bb0a406f01202
Aladine <alazaiane@yahoo.fr>
2014-11-06 10:47:08 +0100

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,18 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def news
+  end
+
+  def organisation
+  end
+
+  def programme
+  end
+
+  def presentation
+  	
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SiteRca::Application.config.secret_key_base = 'f6599cee5790598ea00eb964ab3ff7bdcaf1d37d4ed8deea519db2588d01f078320dbf519d112aad1c142e2574b10596297bb218cbfb6e304062260a3aeed313'
